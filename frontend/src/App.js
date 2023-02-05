54a08c84371a3b99870ea26ec3a72560359e9071
Ravipatel1309 <rpatel8511333@gmail.com>
2023-02-05 23:11:08 +0530

    Login Functionality Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import LandingPage from './screens/LandingPage/LandingPage';
 import MyNotes from './screens/MyNotes/MyNotes';
+import LoginScreen from "./screens/LoginScreens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreens/RegisterScreen";
 
 const App = () => (
   <BrowserRouter>
@@ -13,6 +15,8 @@ const App = () => (
       <main>
 
         <Route exact path='/' component={LandingPage} />
+        <Route exact path='/login' component={LoginScreen} />
+        <Route exact path='/register' component={RegisterScreen} />
         <Route path='/mynotes' component={MyNotes} />
       </main>
     </Switch>
